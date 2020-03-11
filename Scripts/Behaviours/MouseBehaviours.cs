75d0991337adc9a55e9ba21e6ee6c0a7da295e5f
Anthony Romrell <anthonyromrell@mac.com>
2020-03-10 23:36:33 -0600

    Adding Foundation Scripts

---
+++ Scripts/Behaviours/MouseBehaviours.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseBehaviours : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent, mouseEnterEvent;
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+}
---
+++ Scripts/Components/TextBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehaviour : MonoBehaviour
+{
+    private Text textObj;
+    public IntData dataObj;
+    
+    void Start()
+    {
+        textObj = GetComponent<Text>();
+    }
+
+    
+    void Update()
+    {
+        textObj.text = dataObj.value.ToString();
+    }
+    
+}
