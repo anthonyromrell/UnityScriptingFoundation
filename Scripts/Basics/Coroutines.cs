d9f9a5bfa3be077d7b4accdb854458e01ae8962c
Anthony Romrell <anthonyromrell@mac.com>
2020-04-01 13:34:22 -0600

    Coroutines

---
+++ Scripts/Basics/Coroutines.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coroutines : MonoBehaviour
+{
+    public int counter = 3;
+    public float seconds = 1f;
+    public IntData numberData;
+    public UnityEvent startEvent, repeatEvent, endEvent, postEndEvent;
+    private WaitForSeconds waitObj;
+    
+    private IEnumerator Start()
+    {
+        waitObj = new WaitForSeconds(seconds);
+        startEvent.Invoke();
+        while (counter > 0)
+        {
+            numberData.value = counter;
+            yield return waitObj;
+            repeatEvent.Invoke();
+            counter--;
+        }
+        yield return waitObj;
+        endEvent.Invoke();
+        yield return waitObj;
+        postEndEvent.Invoke();
+    }
+}
