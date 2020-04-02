2af4f41748870633fb8ea14bab5e4bb93287b487
Anthony Romrell <anthonyromrell@mac.com>
2020-04-02 16:24:38 -0600

    Major Enhancements to Character and Game Reseting.

--- Scripts/Behaviours/ColliderEventsBehaviour.cs
+++ Scripts/Behaviours/ColliderEventsBehaviour.cs
@@ -1,28 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class ColliderEventsBehaviour : MonoBehaviour
 {
+    public float enterHoldTime, exitHoldTime = 0.1f;
+    public WaitForSeconds enterWaitObj, exitWaitObj;
+
     public UnityEvent triggerEnterEvent, triggerExitEvent, 
         collisionEnterEvent, collisionExitEvent;
 
-    private void OnTriggerEnter(Collider other)
+    private void Awake()
     {
+        enterWaitObj = new WaitForSeconds(enterHoldTime);
+        exitWaitObj = new WaitForSeconds(exitHoldTime);
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        yield return enterWaitObj;
         triggerEnterEvent.Invoke();
     }
     
-    private void OnTriggerExit(Collider other)
+    private IEnumerator OnTriggerExit(Collider other)
     {
+        yield return exitWaitObj;
         triggerExitEvent.Invoke();
     }
 
-    private void OnCollisionEnter(Collision other)
+    private IEnumerator OnCollisionEnter(Collision other)
     {
+        yield return enterWaitObj;
         collisionEnterEvent.Invoke();
     }
     
-    private void OnCollisionExit(Collision other)
+    private IEnumerator OnCollisionExit(Collision other)
     {
+        yield return exitWaitObj;
         collisionExitEvent.Invoke();
     }
 }
