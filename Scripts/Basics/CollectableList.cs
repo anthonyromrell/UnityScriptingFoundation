aef31953edfc25c4cf51325d38356cc17d2db04b
Anthony Romrell <anthonyromrell@mac.com>
2020-04-08 01:19:51 -0600

    Added the Ability to Take Lives and Restart Game.

--- Scripts/Basics/CollectableList.cs
+++ Scripts/Basics/CollectableList.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu]
 public class CollectableList : ScriptableObject
 {
-    public List<Collectable> collectables;
+    public List<Collectible> collectables;
     
     
 }
